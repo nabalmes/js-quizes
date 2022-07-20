39e456e223bf7ccd0ded754c9161cef6ff5f1ad8
Neil Balmes <nabalmes@integranet.ph>
2022-07-20 12:58:09 +0800

    alright

---
+++ README.md
@@ -0,0 +1,14 @@
+# js-quizzes
+
+- Test #01: Odd / Even number
+- Test #02: Prime Numbers in a range of numbers
+- Test #03: Show Factorial of a number
+- Test #04: Reverse a number
+- Test #05: 
+
+#### Instruction for quiz takers:
+- only use file **index-start.html** as base file
+- if you wanna try and solve it, duplicate the base file and rename **index-[your-gitlab-username].html** (e.g.: index-hjvarde.html) and push it.
+- **index-finished.html** contains the answer, up to you if you wanna cheat 😄
+
+Note: feel free to use the markup to add new JavaScript Quizzes.
---
+++ theme.js
@@ -0,0 +1,32 @@
+const systemPrefBtn = document.querySelector('.system-pref-button');
+
+function getCurrentTheme() {
+    let systemPref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    localStorage.getItem('inet.theme') ? systemPref = localStorage.getItem('inet.theme') : null;
+    return systemPref;
+}
+
+function loadSystemPref(systemPref) {
+    const root = document.querySelector(':root');
+    if (systemPref === 'light') {
+        systemPrefBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
+    } else {
+        systemPrefBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
+    }
+    root.setAttribute('color-scheme', `${systemPref}`);
+}
+
+systemPrefBtn.addEventListener('click', () => {
+    let systemPref = getCurrentTheme();
+    if (systemPref=== 'dark') {
+        systemPref = 'light';
+    } else {
+        systemPref = 'dark';
+    }
+    localStorage.setItem('inet.theme', `${systemPref}`);
+    loadSystemPref(systemPref);
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadSystemPref(getCurrentTheme());
+})
